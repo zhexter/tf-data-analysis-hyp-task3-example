02c22d12a1d655cebe1c845117827309f793625b
Eugenia <zhexter@gmail.com>
2023-04-11 15:06:32 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,12 +8,12 @@ def solution(x: np.array, y: np.array) -> bool: # Одна или две выб�
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-#     pvalue = sps.mannwhitneyu(x, y, alternative='less')[1]
+    pvalue = sps.mannwhitneyu(x, y, alternative='less')[1]
 #     pvalue = sps.ttest_ind(x, y, equal_var=False, alternative='less').pvalue
-    pvalue = sps.permutation_test((x, y), 
-                                  lambda x, y, axis: np.mean(x, axis=axis) - np.mean(y, axis=axis),
-                                  vectorized=True,
-                                  n_resamples=1000,
-                                  alternative='less').pvalue
+#     pvalue = sps.permutation_test((x, y), 
+#                                   lambda x, y, axis: np.mean(x, axis=axis) - np.mean(y, axis=axis),
+#                                   vectorized=True,
+#                                   n_resamples=1000,
+#                                   alternative='less').pvalue
     alpha = 0.09
     return pvalue < alpha # Ваш ответ, True или False
